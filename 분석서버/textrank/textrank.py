54b78408d73bf1d764b7d8a033180510f934b6fe
redbird <cs20131575@u.sogang.ac.kr>
2018-03-26 21:43:49 +0900

    modified textrank

--- 분석서버/textrank/textrank.py
+++ 분석서버/textrank/textrank.py
@@ -13,7 +13,7 @@ def repl(m):
     return ' ' * len(m.group())
 # keywords, sentences
 class TextRank:
-    # 박차장님께서 도와주신 전처리 함수입니다.
+
     def preprocess(self, text):
 
         target_list = ["\t", "…", "·", "●", "○", "◎", "△", "▲", "◇", "■", "□", ":phone:", "☏", "※", ":arrow_forward:", "▷", "ℓ", "→", "↓", "↑", "┌", "┬", "┐", "├", "┤", "┼", "─", "│", "└", "┴", "┘"]
@@ -69,7 +69,6 @@ class TextRank:
         return text
 
     # quote remover and line splitter  result convert
-    # 인용구 지웠던 것을 다시 복구 시킨다. (인용구 지우는 이유 : 라인을 좀 더 쉽게 자르기 위해)
     def convert_to_original_string(self, strings, original):
         idx = 0
         res = []
@@ -134,13 +133,10 @@ class TextRank:
             self.dictNear[a, b] = self.dictNear.get((a, b), 0) + 1
 
         taggerIter = self.readTagger()
-        taggerArr = list(map(lambda x: list(x), taggerIter))
-
         # save pos tagging result
-        self.pos = list(itertools.chain(*taggerArr))
         wordFilter = self.taggerTokenizer
 
-        for sent in taggerArr:
+        for sent in taggerIter:
             sent = list(filter(self.englishFilter, sent))
             for i, word in enumerate(sent):
 
@@ -261,14 +257,12 @@ class TextRank:
     # get keywords 
     # 키워드 추출
     def keywords(self, num=15):
-        ranks = self.keyword_ranks ## 구성한 그래프를 가져온다. (페이지 랭크)
-        wordFilter = self.taggerTokenizer ## 단어 추출 기준( pos tagging 된 것 중에 어떤게 키워드 추출할 수 있는 단어인지 구분)
-        cand = sorted(ranks, key=ranks.get, reverse=True) ## 정렬
+        ranks = self.keyword_ranks
+        wordFilter = self.taggerTokenizer
+        cand = sorted(ranks, key=ranks.get, reverse=True)
         pairness = { }
         startOf = { }
         tuples = { }
-
-        # unigram 추출 및 PMI 계산
         for k in cand:
             if k[1] != 'VA' and k[1] != 'VV':
                 tuples[(k, )] = self.getI(k) * ranks[k]
@@ -282,9 +276,7 @@ class TextRank:
         for (k, l) in sorted(pairness, key=pairness.get, reverse=True):
             if k not in startOf:
                 startOf[k] = (k, l)
-        #---------------------------unigram 추출 및 PMI 계산 여기까지
 
-        # bigram 추출합니다.
         for (k, l), v in pairness.items():
             pmis = v
             rs = ranks[k] * ranks[l]
@@ -299,8 +291,7 @@ class TextRank:
                 rs *= ranks[last]
                 path += (last, )
                 # G(TR) * A(PMI) * Length
-                # 이 수식으로 bigram 추출, 밑에 한 줄 코드가 이 수식임. (참고. 나눗셈이 나오는 건 산술평균)
-                tuples[path] = pmis / (len(path) - 1) * res ** (1 / len(path)) * len(path)
+                tuples[path] = pmis / (len(path) - 1) * rs ** (1 / len(path)) * len(path)
 
         used = set()
         both = { }
@@ -314,26 +305,17 @@ class TextRank:
         unigram = []
         bigram = []
         avg = 0.0
-
-        # cost의 평균을 계산 
         for key in both.keys():
             avg += both[key]
         if len(both.keys()) > 0:
             avg /= len(both.keys())
 
-        # 제목 단어 추출
         titleWords = list(filter(wordFilter, self.readTaggerForTitle()))
         titleWords = list(map(lambda x: x[0], titleWords))
-        #--------제목 단어 추출 여기까지
-
-        # unigram 및 bigram을 본문에 나오는 형태로 저장 및 제목 가중치 조절 및 빈도 수 가중치 조절이 같이 들어가 있음
         for key in both.keys():
             if len(key) == 2:
-                # 특정 빈도수 이하는 가중치 뺄셈
                 if self.taggerDictCount[key[0]] <= self.minimum_low_freq or self.taggerDictCount[key[1]] <= self.minimum_low_freq:
                     both[key] -= avg * self.low_freq_word_subtraction_multiplier
-
-                # 제목에 등장한 단어 가중치 추가
                 if key[0][0] in titleWords or key[1][0] in titleWords:
                     both[key] += avg * self.title_word_addition_multiplier
                 joined1 = '%s %s' % (key[0][0], key[1][0])
@@ -352,24 +334,15 @@ class TextRank:
                 if key[0][1] != 'SN':
                     unigram.append(('%s' % (key[0][0]), both[key]))
 
-        # cost가 음수가 되는 것들 제거
-        unigram = list(filter(lambda x: x[1] > 0.0, unigram))
-        bigram = list(filter(lambda x: x[1] > 0.0, bigram))
+        unigram = list(filter(lambda x: x[1] > 0.0, unigram))[:num]
+        bigram = list(filter(lambda x: x[1] > 0.0, bigram))[:num]
 
 
         res = set([])
+        ## 공백없는 복합단어를 합친다. ex) 한양대 대나무/숲/향기 -> 한양대 대나무숲향기
+        bigram, ex_words = self.expand_bigram_to_match_context(bigram)
+        unigram = list(filter(lambda x: x[0] not in ex_words, unigram))
 
-        # 본문 명사 단어를 추출
-        words = list(map(lambda x: (x, 0), self.tagger.nouns(self.preprocess(self.content))))
-
-        ## 공백없는 복합단어를 최대 2개까지 합친다. ex) 한양대 대나무/숲/향기 -> 한양대 대나무숲향기
-        ## 현재는 4개
-        for _ in range(4):
-            bigram, ex_words = self.merge_n_gram_with_unigram(bigram, words)
-            unigram = list(filter(lambda x: x[0] not in ex_words, unigram))
-
-        # 정렬 후 num 개수 만큼 추출
-        # 여기서 num은 tr.keywords(num=15) <-- 여기의 num 변수
         res = set(unigram + bigram)
         res = list(filter(lambda x: x[1] > 0.0, res))
         res = sorted(res, key=lambda x: x[1], reverse=True)[:num]
@@ -377,37 +350,56 @@ class TextRank:
         return res
 
 
-    ## 합친 단어가 전부 명사인지 다시한번 체크해주는 함수 (예: 다이어트 핵심이(X) 다이어트 핵심법(O)
-    def check_word_is_noun(self, word):
-        words = self.tagger.pos(word)
-        return len(list(filter(lambda x: self.nounChecker(x[1]), words))) == len(words)
-
 
     ## 본문에 나온 형태로 띄어쓰기 없는 명사단어를 붙여주는 함수
-    def merge_n_gram_with_unigram(self, n_gram, nouns):
+    def expand_bigram_to_match_context(self, n_gram):
 
         res = []
         ex_words = set([])
         for words in n_gram:
-            found = False
-            for word in nouns:
-                ## 본문에는 붙어나오지만 형태소 분석기때문에 잘린 단어를 복구
-                prefix_joined = word[0] + words[0]
-                suffix_joined = words[0] + word[0]
-        
-                # 합칠 때 공백을 최대 1개만 허용
-                if prefix_joined in self.content and self.check_word_is_noun(prefix_joined):
-                    found = True
-                    res.append((prefix_joined, word[1] + words[1])) # cost sum
-                    ex_words.add(word[0])
-                elif suffix_joined in self.content and self.check_word_is_noun(suffix_joined):
-                    found = True
-                    res.append((suffix_joined, word[1] + words[1])) # cost sum
-                    ex_words.add(word[0])
-            if not found:
-                res.append(words)
-                
-        # 합친 결과, ex_word(합쳐져서 제외 할 단어) return
+
+            # words 를 본문에서 찾아서 공백이 없을때까지 다 합친다. 그리고 words 에서 나온 단어를 제외하고 붙인다.
+
+            position = self.content.find(words[0])
+            left = position - 1
+            right = position + len(words[0])
+
+            while left >=0 and self.content[left] != ' ': left -= 1
+            while right < len(self.content) and self.content[right] != ' ': right += 1
+
+            morph = self.content[left: right+1]
+            poses = self.tagger.pos(morph)
+
+            left = -1
+            right = len(poses)
+            for idx, pos in enumerate(poses):
+                if idx == len(poses)-1:
+                    break
+                joined1 = poses[idx][0] + poses[idx+1][0]
+                joined2 = poses[idx][0] + ' ' + poses[idx+1][0]
+                if joined1 == words[0] or joined2 == words[0]:
+                    left = idx - 1
+                    right = idx + 2
+
+            lower = 0
+            upper = len(poses)-1
+
+            merged_bigram = words[0]
+
+            while lower <= left:
+                if self.nounChecker(poses[lower][1]):
+                    merged_bigram = poses[lower][0] + merged_bigram
+                    ex_words.add(poses[lower][0])
+                lower += 1
+
+            while right <= upper:
+                if self.nounChecker(poses[upper][1]):
+                    merged_bigram = merged_bigram + poses[upper][0]
+                    ex_words.add(poses[lower][0])
+                upper -=1
+
+            res.append((merged_bigram, words[1]))
+
         return res, ex_words
 
     # pagerank apply
@@ -428,16 +420,12 @@ class TextRank:
 
     # 숫자 + 명사를 한 단어로 인식하게 하는 함수
     def numeric(self, sent):
-        # sent -> 한 문장
-        # 8500만원 => 8500:SN 만:NR 원:NNBC 명사긴 한데 이상한 명사라서 뒤에 다 합침
-        words = self.tagger.pos(sent)
 
-        # 반복문으로 안나올때까지 합침
+        words = self.tagger.pos(sent)
         for idx, word in enumerate(words):
             if idx == 0:
                 continue
 
-            
             prev_word = words[idx-1]
             first_word_used = False
             # number + noun
@@ -475,7 +463,7 @@ class TextRank:
         return words
 
     def __init__(self, **kwargs):
-        # method overidng 
+
         self.graph = None
         self.tagger = kwargs.get('tagger', Mecab())
         self.window = kwargs.get('window', 5)
@@ -501,10 +489,10 @@ class TextRank:
         self.dictNear = { }
         self.nTotal = 0
 
-        # only extract keyword NNG, NNP (일반 명사, 고유 명사, 숫자, 한자 제외), 영어일 경우 영어 명사 사전에 등록된 단어인지 검색
+        # only extract keyword NNG, NNP (일반 명사, 고유 명사, 외국어, 숫자, 한자 제외), 영어일 경우 영어 명사 사전에 등록된 단어인지 검색
         self.nounChecker = lambda x: x in ('NNG', 'NNP', 'SL')
         self.taggerTokenizer = lambda x: x[0] not in self.stopwords and self.nounChecker(x[1]) and (len(x[0]) > 1 or x[0] in self.singlewords) and re.compile('[一-龥]+').match(x[0]) is None
-        # 영어일 경우 명사 사전에 등록된 단어인지 검색
+
         self.englishFilter = lambda x: x[1] != 'SL' or nltk.pos_tag([x[0]])[0][1][0] == 'N'
 
         # variables for sentence summarization
